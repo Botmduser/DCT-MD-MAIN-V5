22d8a736a290d7c7b7b4ed95f40549836657a617
Botmduser <Botdeploy698@gmail.com>
2026-06-14 18:03:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || "youre session id",
-    MONGODB: process.env.MONGODB || "enter mongodb here",
+    MONGODB: process.env.MONGODB || "mongodb+srv://bepigi3013_db_user:lVS7vHg3uhzFjJDG@hashuserver2.wjpqr2x.mongodb.net/?appName=hashuserver2",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/PwTkwNQ/20241209-212640.jpg",
     PREFIX: process.env.PREFIX || ".",
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
